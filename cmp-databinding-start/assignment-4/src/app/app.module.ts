363a39b5508f6ac0bde51279edfb54cf646ca702
Anuj Kumar Singh <52523854+000GEV744@users.noreply.github.com>
2022-02-28 21:54:22 +0530

    completed assignment-4

--- cmp-databinding-start/assignment-4/src/app/app.module.ts
+++ cmp-databinding-start/assignment-4/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GameControlComponent } from './game-control/game-control.component';
+import { GameOddComponent } from './game-odd/game-odd.component';
+import { GameEvenComponent } from './game-even/game-even.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameControlComponent,
+    GameOddComponent,
+    GameEvenComponent
   ],
   imports: [
     BrowserModule
